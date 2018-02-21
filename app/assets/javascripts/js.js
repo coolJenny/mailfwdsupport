658b707ef3af4123e2dced898eef83f0b151c574
coolJenny <coolJenny009g@gmail.com>
2018-02-21 07:06:45 -0500

    ..

---
+++ app/assets/javascripts/js.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+	$('.clickable').click(function(e){
+		var $this = $(this);
+		if(!$this.hasClass('panel-collapsed')) {
+			$this.parents('.panel').find('.panel-body').slideUp();
+			$this.addClass('panel-collapsed');
+			$this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+		} else {
+			$this.parents('.panel').find('.panel-body').slideDown();
+			$this.removeClass('panel-collapsed');
+			$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+		}
+	});
+
+	$('.add-tag').hide();
+	$('.add-tag-btn').click(function(e){
+		$('.add-tag').show();
+	});
+
+	$('.add-keyword').click(function() {
+		var keyword_val = $('.keyword_val').val();
+		if(keyword_val){
+			$('.keyword_val').css({"border":""});
+			$('.keywords').append('<span class="btn btn-primary keyword-span ' + 'keyword-' + keyword_val + '">' + keyword_val + '</span>');
+		}else{
+			$('.keyword_val').css({"border":"1px solid red"});
+			alert("Please type your Keyword!");			
+		}
+	});
+	
+});
